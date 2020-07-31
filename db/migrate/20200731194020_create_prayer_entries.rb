a2a6a48411fd557068efe9b0fe119eba09280e2f
Donna Torres <todonnatorres@icloud.com>
2020-07-31 14:55:07 -0500

    add authentication and timestamps

--- db/migrate/20200731194020_create_prayer_entries.rb
+++ db/migrate/20200731194020_create_prayer_entries.rb
@@ -1,6 +1,8 @@
 class CreatePrayerEntries < ActiveRecord::Migration
   def change
     create_table :prayer_entries do |t|
+      t.string :content
+      t.integer :user_id
 
       t.timestamps null: false
     end
