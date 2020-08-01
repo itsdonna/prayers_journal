3df74f9ad8cdf97eded2d24d398a4e927dc35c55
Donna Torres <todonnatorres@icloud.com>
2020-07-31 23:59:54 -0500

    migrate and seed db

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+#create users and prayer entries
+
+abraham = User.create(name: "Abraham", email: "abraham@me.com", password: "test2468")
+joe = User.create(name: "Joe", email: "joe@me.com", password: "test2468")
+
+#prayer entries
+PrayerEntry.create(content: "Today I am grateful to wake up.", user_id: abraham.id )
+
+abraham.prayer_entries.create(content: "Today is a great day to rejoice!")
+
+#build instantiates similar to using new so we have to call save.
+joes_entry = joe.prayer_entries.build(content: "I am grateful for the breeze during a hot summer walk.")
+joes_entry.save 
