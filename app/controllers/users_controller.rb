684e2d816f9ddcf331e1c01a80fc6a7e50fefcfb
Donna Torres <todonnatorres@icloud.com>
2020-08-01 23:29:05 -0500

    added get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,37 @@
 class UsersController < ApplicationController
 
-    #login route -> login page
+    #create: login route -> login page
     get '/login'do
        erb :login
     end
 
-    #signup route
+    #create session: find user, login
+    #form action ->login (url), method ->post
+    post '/login' do
+
+        @user = User.find_by(email: params[:email])
+        
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id #each user has a unique id to login
+            #redirect to landing page
+            puts session
+            redirect "users/#{@user.id}"
+        
+
+        else
+        end
+        
+    end
+
+    #create :signup route
     get '/signup'do
         
     end
 
-    #
+    get '/users/:id' do
+        "show route"
+    end
+
+    
 
 end
