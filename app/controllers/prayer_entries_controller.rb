70745e4d5ab2b872bf9bfc6273160e2384142b6d
Donna Torres <todonnatorres@icloud.com>
2020-08-03 16:46:53 -0500

    add prayer entries and show route

--- app/controllers/prayer_entries_controller.rb
+++ app/controllers/prayer_entries_controller.rb
@@ -5,6 +5,21 @@ class PrayerEntriesController < ApplicationController
     end
 
     post '/prayer_entries' do
-        
+        #create new entry
+        #save entry
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:content] != ""
+            @prayer_entry = PrayerEntry.create(content: params[:content], user_id: current_user.id)
+            redirect "/prayer_entries/#{@prayer_entry.id}"
+        else
+            redirect '/prayer_entries/new'
+        end
+    end
+
+    get 'prayer_entries/:id' do
+        @prayer_entry = PrayerEntry.find(params[:id])
+        erb :'/prayer_entries/show'
     end
 end
