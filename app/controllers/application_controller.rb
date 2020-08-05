8fc4254e16ec893e100816f509c907c7833e0939
Donna Torres <todonnatorres@icloud.com>
2020-08-05 01:22:10 -0500

    add helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,9 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by(id: session[:user_id])
     end
 
+    def authorized_to_edit?(prayer_entry)
+       prayer_entry.user == current_user
+    end
     
   end
 
