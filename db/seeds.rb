153839b21a2151acb075935b0214f4ef50505d7a
Donna Torres <todonnatorres@icloud.com>
2020-08-14 18:14:06 -0500

    added title column

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ abraham = User.create(name: "Abraham", email: "abraham@me.com", password: "test2
 joe = User.create(name: "Joe", email: "joe@me.com", password: "test2468")
 
 #prayer entries
-PrayerEntry.create(content: "Today I am grateful to wake up.", user_id: abraham.id )
+PrayerEntry.create(title: "Grateful", content: "Today I am grateful to wake up.", user_id: abraham.id )
 
-abraham.prayer_entries.create(content: "Today is a great day to rejoice!")
+abraham.prayer_entries.create(title: "Rejoice", content: "Today is a great day to rejoice!")
 
 #build = instantiates, similar to using new so we have to call save.
-joes_entry = joe.prayer_entries.build(content: "I am grateful for the breeze during a hot summer walk.")
+joes_entry = joe.prayer_entries.build(title: "Cool mornings", content: "I am grateful for the breeze during a hot summer walk.")
 joes_entry.save 
