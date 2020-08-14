e4f05bdd9576db0624123240d05aa9e7cd38dccc
Donna Torres <todonnatorres@icloud.com>
2020-08-14 18:14:51 -0500

    added title column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200731194020) do
+ActiveRecord::Schema.define(version: 20200814045612) do
 
   create_table "prayer_entries", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
