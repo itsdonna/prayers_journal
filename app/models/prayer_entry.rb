fa07b13e785fdcc3b5c6841239fce204cda80f12
Donna Torres <todonnatorres@icloud.com>
2020-08-14 18:15:56 -0500

    add flash messages and title column

--- app/models/prayer_entry.rb
+++ app/models/prayer_entry.rb
@@ -1,5 +1,9 @@
 class PrayerEntry < ActiveRecord::Base
 
     belongs_to :user
+
+    def formatted_created_at
+        self.created_at.strftime("%a, %d %b %Y")
+    end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password #allows us to authenticate
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 
     has_many :prayer_entries
     
